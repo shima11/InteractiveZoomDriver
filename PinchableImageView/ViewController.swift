546b8baa33b6e829775ce7b0b184d79bd9709920
Shima <shima.jin@icloud.com>
2018-04-16 12:37:26 +0900

    Initial Commit

---
+++ PinchableImageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinchableImageView
+//
+//  Created by Jinsei Shima on 2018/04/16.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
